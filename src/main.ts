5b2ff47a41b42ef9a895507bc897787dec80f667
Rodrigo Avila G <rodrigoavilag12@gmail.com>
2026-04-08 13:20:29 -0600

    feat: enabling cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common'; // <-- 1. Importamos esto
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors();
   // 2. Activamos la validación global
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // Esto elimina automáticamente cualquier dato basura que nos envíen y no esté en el DTO
